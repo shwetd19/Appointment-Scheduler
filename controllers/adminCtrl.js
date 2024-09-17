ac93d725084dc3f990b765bdef3e6cbe74e41376
Shwetas Dhake <shwetas.dhake@mitaoe.ac.in>
2024-09-17 20:03:26 +0530

    Update adminCtrl.js

--- controllers/adminCtrl.js
+++ controllers/adminCtrl.js
@@ -6,15 +6,15 @@ const getAllUsersController = async (req, res) => {
     const users = await userModel.find({});
     res.status(200).send({
       success: true,
-      message: "users data list",
+      message: "Users data list",
       data: users,
     });
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
-      message: "erorr while fetching users",
-      error,
+      message: "Error while fetching users",
+      error: error.message,
     });
   }
 };
@@ -24,44 +24,56 @@ const getAllDoctorsController = async (req, res) => {
     const doctors = await doctorModel.find({});
     res.status(200).send({
       success: true,
-      message: "Doctors Data list",
+      message: "Doctors data list",
       data: doctors,
     });
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
-      message: "error while getting doctors data",
-      error,
+      message: "Error while getting doctors data",
+      error: error.message,
     });
   }
 };
 
-// doctor account status
+// Doctor account status
 const changeAccountStatusController = async (req, res) => {
   try {
     const { doctorId, status } = req.body;
-    const doctor = await doctorModel.findByIdAndUpdate(doctorId, { status });
-    const user = await userModel.findOne({ _id: doctor.userId });
-    const notifcation = user.notifcation;
-    notifcation.push({
+    const doctor = await doctorModel.findByIdAndUpdate(doctorId, { status }, { new: true });
+    if (!doctor) {
+      return res.status(404).send({
+        success: false,
+        message: "Doctor not found",
+      });
+    }
+    const user = await userModel.findById(doctor.userId);
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "User not found",
+      });
+    }
+    const notification = user.notification || [];
+    notification.push({
       type: "doctor-account-request-updated",
-      message: `Your Doctor Account Request Has ${status} `,
+      message: `Your Doctor Account Request Has Been ${status}`,
       onClickPath: "/notification",
     });
-    user.isDoctor = status === "approved" ? true : false;
+    user.isDoctor = status === "approved";
     await user.save();
-    res.status(201).send({
+    res.status(200).send({
       success: true,
-      message: "Account Status Updated",
+      message: "Account status updated",
       data: doctor,
     });
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
-      message: "Eror in Account Status",
-      error,
+      message: "Error in updating account status",
+      error: error.message,
     });
   }
 };
